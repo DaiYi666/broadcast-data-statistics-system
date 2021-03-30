78d3830ad558a91d3d2e9990304f98661bf34e56
DaiYi <898469219@qq.com>
2021-03-30 14:52:53 +0800

    2021/2/30

--- src/main/java/com/scfenzhi/configuration/FilterConfig.java
+++ src/main/java/com/scfenzhi/configuration/FilterConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
     @Bean
-    public FilterRegistrationBean reqResFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+    public FilterRegistrationBean<EquipmentFilter> reqResFilter() {
+        FilterRegistrationBean<EquipmentFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new EquipmentFilter());
         filterRegistrationBean.addUrlPatterns("/*");//配置过滤规则 　　　　 return filterRegistrationBean;
         return filterRegistrationBean;
--- src/main/java/com/scfenzhi/filter/EquipmentFilter.java
+++ src/main/java/com/scfenzhi/filter/EquipmentFilter.java
@@ -8,33 +8,12 @@ public class EquipmentFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         String userAgent = ((HttpServletRequest) servletRequest).getHeader("User-Agent");
         System.out.println("设备类型：" + userAgent);
-        if (userAgent.contains("Windows")){
-            filterChain.doFilter(servletRequest,servletResponse);
-        }else {
-            servletRequest.getRequestDispatcher("/").forward(servletRequest,servletResponse);
+        if (userAgent.contains("Windows")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            servletRequest.getRequestDispatcher("/errorPage/deviceErrorPage.html").forward(servletRequest, servletResponse);
         }
-
-//        if (User_Agent.contains("Android") || User_Agent.contains("Linux")) {
-//            System.out.println("Android移动客户端");
-//            if (User_Agent.contains("MicroMessenger")) {
-//                System.out.println("微信");
-//            }
-//        } else if (User_Agent.contains("iPhone")) {
-//            System.out.println("iPhone移动客户端");
-//            if (User_Agent.contains("MicroMessenger")) {
-//                System.out.println("微信");
-//            }
-//        } else if (User_Agent.contains("iPad")) {
-//            System.out.println("iPad客户端");
-//            if (User_Agent.contains("MicroMessenger")) {
-//                System.out.println("微信");
-//            }
-//        } else if (User_Agent.contains("Windows")) {
-//            System.out.println("Windows");
-//        }
-
     }
 }
